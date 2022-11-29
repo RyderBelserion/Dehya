f4d65a4f6203d9e732f13e605dc6765baadb523e
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-11-29 16:48:29 -0500

    Bump kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
 
     id("com.github.johnrengelman.shadow") version "7.1.2"
 
@@ -30,9 +30,7 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.4.5")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-
-    implementation(kotlin("stdlib", "1.7.20"))
+    implementation(kotlin("stdlib", "1.7.21"))
 }
 
 tasks {
