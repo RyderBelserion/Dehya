f162cc5b24d0e78acadd2ade05dcdba9c49efdc0
Ryder Belserion <no-reply@ryderbelserion.com>
2022-12-13 17:49:07 -0500

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 
 val buildNumber: String? = System.getenv("BUILD_NUMBER")
 
-val jenkinsVersion = "0.0.1-b$buildNumber"
+project.group = "net.dehya.dehyabot"
+project.version = "0.0.1"
+project.description = "A discord bot in kotlin using the Discord JDA library."
 
-group = "net.dehya.dehyabot"
-version = "0.0.1"
-description = "A discord bot in kotlin using the Discord JDA library."
+val buildVersion = "${project.version}-b$buildNumber"
 
 application {
     mainClass.set("net.dehya.dehyabot.Dehya")
@@ -23,11 +23,15 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-beta.1")
+    implementation("net.dv8tion", "JDA", "5.0.0-beta.1")
 
-    implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
+    implementation("ch.qos.logback", "logback-classic", "1.4.5")
 
-    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("io.github.cdimascio", "dotenv-kotlin", "6.4.0")
+
+    implementation("org.jetbrains.exposed", "exposed-core", "0.40.1")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.40.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.40.1")
 
     implementation(kotlin("stdlib", "1.7.21"))
 }
@@ -35,16 +39,16 @@ dependencies {
 tasks {
     shadowJar {
         if (buildNumber != null) {
-            archiveFileName.set("${rootProject.name}-[v${jenkinsVersion}].jar")
+            archiveFileName.set("${rootProject.name}-${buildVersion}.jar")
         } else {
-            archiveFileName.set("${rootProject.name}-[v${rootProject.version}].jar")
+            archiveFileName.set("${rootProject.name}-${project.version}.jar")
         }
 
-        listOf(
-            "net.dv8tion"
-        ).onEach {
-            relocate(it, "${group}.libs.$it")
-        }
+        //listOf(
+        //    "net.dv8tion"
+        //).onEach {
+        //    relocate(it, "${group}.libs.$it")
+        //}
     }
 
     compileKotlin {
